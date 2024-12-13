7964d92ef7aef2ff6ec404184c22f9ca271830f5
birutalukas <biruta.lukas@gmail.com>
2024-12-13 09:56:38 +0200

    Test Lenis:

--- src/scripts/modules/lenisSetup.js
+++ src/scripts/modules/lenisSetup.js
@@ -1,6 +1,9 @@
 import Lenis from "lenis";
 
 export const lenis = new Lenis({
+    smooth: true,
+    smoothTouch: true,
+    direction: "vertical",
     prevent: (node) => node.classList.contains("side-overlay"),
 });
 
